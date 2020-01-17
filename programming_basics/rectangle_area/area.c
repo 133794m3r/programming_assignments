472057e1a984ca8ec7f3d663e45534568d3ce22b
Macarthur Inbody <133794m3r@gmail.com>
2020-01-17 16:34:53 -0500

    moved to proper area.

---
+++ programming_basics/rectangle_area/area.c
@@ -0,0 +1,34 @@
+/*
+* Area of a rectangle calculator.
+* Done entirely in C.
+*/
+//Have to include the IO functions.
+#include <stdio.h>
+//main either should be void or an int.
+int main(){
+	//initialize base as an unsigned long integer and set the value to 0.
+	unsigned long base=0;
+	//same for height.
+	unsigned long height=0;
+	//same again.
+	unsigned long area=0;
+	//Print out to the console the the sentence.
+	printf("Rectangle area calculator.\n");
+	//Print out the "prompt".
+	printf("Enter the base: ");
+	//We're going to look for user input then we'll pass
+	//the result to the value pointed to at the address that we give it.
+	//Also it casts it to an unsigned long value.
+	scanf("%lu",&base);
+	//We do the same thing again for height.
+	printf("Enter the height: ");
+	scanf("%lu",&height);
+	//get the area value.
+	area=base*height;
+	//Here we'll print out the area. the %lu is the formatter like in python
+	//you can format the output of any value given to it. But in C you have
+	//pass it after a comma. We also print out a new line.
+	printf("The area is %lu square units.\n",area);
+	//return zero because it exited OK.
+	return 0;
+}
---
+++ programming_basics/rectangle_area/area.js
@@ -0,0 +1,26 @@
+/*
+* Area of a Rectangle JavaScript Version.
+* The following is a basic version of the same script but in JavaScript.
+*/
+//declare base to zero.
+var base=0;
+//same again.
+var height=0;
+//and a third time.
+var area=0;
+//going to have to alert unless they want to install node.
+alert("Area of a Rectangle calculator.")
+//Here we prompt the user to enter a string. Then we take that value turn it into an Integer.
+//Then we set the value to be base.
+base=parseInt(prompt("Enter the base: "))
+//same as height.
+height=parseInt(prompt("Enter the height: "))
+//simply multiply them together.
+area=base*height
+//alert the value to them.
+//JavaScript is fine with adding strings and "Ints"* together in a string. It doesn't care.
+alert("The area is "+area+" square units.")
+
+
+//* Actually all numeric values in javascript are stored as signed "integers". In reality they are really
+//doubles but are shown as either floating point or are floored to be shown to you.
