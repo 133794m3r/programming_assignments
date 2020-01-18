bce32f4c8c40b666e2a7bd63ac31479093dccc43
Macarthur Inbody <133794m3r@gmail.com>
2020-01-17 21:53:05 -0500

    Renamed a.c to pointer_arithematic.c also moved areas to proper name.

--- a.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-char a[] = "\";\n void main(){char *b=a;printf(\"#include<stdio.h>\\nchar a[] = \\\"\");for(;*b;b++) {switch(*b){case '\\n': printf(\"\\\\n\"); break: \ncase '\\\\': case '\\\":putchar('\\\\’); default: putchar(*b);}} printf(a);}\n"; 
-void main() {
-char *b=a;
-printf("#include <stdio.h>\nchar a[] = \""); for(;*b;b++) {switch(*b){case '\n':printf("\\n"); break; case '\\': case '\'': putchar('\\'); default: putchar(*b);}} printf(a);printf("\nStrength through pointer arithematic\n");}
-
-
---
+++ pointer_arithematic.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+char a[] = "\";\n void main(){char *b=a;printf(\"#include<stdio.h>\\nchar a[] = \\\"\");for(;*b;b++) {switch(*b){case '\\n': printf(\"\\\\n\"); break: \ncase '\\\\': case '\\\":putchar('\\\\’); default: putchar(*b);}} printf(a);}\n";
+void main() {char *b=a;printf("#include <stdio.h>\nchar a[] = \"");for(;*b;b++) {switch(*b){case '\n':printf("\\n"); break; case '\\': case '\'':putchar('\\');default: putchar(*b);}}printf(a);printf("\nStrength through pointer arithematic\n");}
