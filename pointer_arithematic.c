d0790123a06aa4836c8988a9c474db43d9daac74
Macarthur Inbody <133794m3r@gmail.com>
2020-01-18 00:58:27 -0500

    quine wasn't printing itself actually.

--- pointer_arithematic.c
+++ pointer_arithematic.c
@@ -1,3 +1,4 @@
 #include <stdio.h>
-char a[] = "\";\n void main(){char *b=a;printf(\"#include<stdio.h>\\nchar a[] = \\\"\");for(;*b;b++) {switch(*b){case '\\n': printf(\"\\\\n\"); break: \ncase '\\\\': case '\\\":putchar('\\\\’); default: putchar(*b);}} printf(a);}\n";
-void main() {char *b=a;printf("#include <stdio.h>\nchar a[] = \"");for(;*b;b++) {switch(*b){case '\n':printf("\\n"); break; case '\\': case '\'':putchar('\\');default: putchar(*b);}}printf(a);printf("\nStrength through pointer arithematic\n");}
+char a[] = "\";\nint main(){char *b=a;printf(\"#include<stdio.h>\\nchar a[] = \\\"\");for(;*b;b++) {switch(*b){case '\\n': printf(\"\\\\n\"); break; \ncase '\\\\': case '\\\"':putchar('\\\\'); default: putchar(*b);}} printf(a); return 0;}\n";int main() {char *b=a;
+printf("#include <stdio.h>\nchar a[] = \"");for(;*b;b++) {switch(*b){case '\n':
+printf("\\n"); break; case '\\': case '"':putchar('\\'); default: putchar(*b);}}printf(a);printf("\nStrength through pointer arithmetic\n"); return 0;}
